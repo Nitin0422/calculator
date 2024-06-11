2017198fd0fe7ed12c4ee6f103b838fa7b6948ec
nitintandukar <nitin.tandukar0422@gmail.com>
2024-06-11 14:34:47 +0545

    Initial layout madea

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 function App() {
   return (
-    <div className="min-w-screen min-h-screen flex flex-col justify-around">
+    <div className="min-w-screen min-h-screen flex flex-col justify-center items-center bg-neutral-900 gap-8">
       <div className="flex justify-center">
-        <h1> Simple React Calculator</h1>
+        <h1 className="text-2xl font-bold text-white"> Simple React Calculator</h1>
       </div>
-      <div>
-        Here will be the main components
+      <div className=" bg-neutral-800 flex flex-col gap-4 rounded-xl p-5">
+        <div className="p-2 py-4 text-white text-xl text-right bg-neutral-900 rounded-xl">
+          0
+        </div>
+        <div className="buttons grid grid-rows-4 grid-cols-4 gap-4">
+        <input type="button" value="7" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+
+          <input type="button" value="8" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="9" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="AC" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="4" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="5" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="6" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="-" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="1" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="2" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="3" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="+" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="/" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="0" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="*" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+          <input type="button" value="=" className="bg-neutral-900 rounded-full p-3 shadow-lg shadow-neutral-600 font-thin text-3xl text-white hover:bg-neutral-800 focus:outline-none transition duration-100 transform-gpu active:scale-95" />
+
+
+        </div>
       </div>
     </div>
   )
